0e99e1fec6bfa9790ba38f86085cc69300ce8605
Sukalyan <sukalyandash1920@gmail.com>
2022-02-11 17:38:37 +0530

    ADD: UC1 - Load, analyse the Indian States Census Information from a CSV file completed

---
+++ CensusAnalyzerProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CensusAnalyzerProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Welcome to Indian States Census Analyser program \n");
+        }
+    }
+}
